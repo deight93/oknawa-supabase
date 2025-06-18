b0ad443baf0ca744eb80843c8a392b954191338d
deight93 <deight8311@gmail.com>
2025-06-18 15:15:37 +0900

    feat: location-together 개발

--- test.sh
+++ test.sh
@@ -13,9 +13,9 @@ curl -X POST http://localhost:54321/functions/v1/location-points \
   -H "Authorization: Bearer SUPABASE_ACCESS_TOKEN" \
   -d '{
     "participant": [
-      { "name": "김동환", "start_x": 127.129347097241, "start_y": 37.4860140096538 },
-      { "name": "김디팔", "start_x": 127.113273744844, "start_y": 37.5064595683176 },
-      { "name": "김띨팔", "start_x": 126.917252, "start_y": 37.494990 }
+      { "name": "김동환", "region_name": "A동", "start_x": 127.129347097241, "start_y": 37.4860140096538 },
+      { "name": "김디팔", "region_name": "B 장소", "start_x": 127.113273744844, "start_y": 37.5064595683176 },
+      { "name": "김띨팔", "region_name": "C장소", "start_x": 126.917252, "start_y": 37.494990 }
     ]
   }'
 
@@ -39,8 +39,8 @@ curl -X POST "http://localhost:54321/functions/v1/location-points-vote/{MAP_ID}"
 # 5. POST location/points/{map_id}/confirm (TABLE API)
 curl --request PATCH \
   'http://127.0.0.1:54321/rest/v1/location_result?map_id=eq.73f8aaaa-e86e-46ca-8622-3b7ca570b8a5&map_host_id=eq.aaaa8f37' \
-  -H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0" \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0" \
+  -H "apikey: SUPABASE_ACCESS_TOKEN" \
+  -H "Authorization: Bearer SUPABASE_ACCESS_TOKEN" \
   -H "Content-Type: application/json" \
   -d '{ "confirmed": "05a8662d-08e8-4971-b77a-866c8d4f9c2f" }'
 
@@ -50,3 +50,15 @@ curl --request GET 'http://127.0.0.1:54321/rest/v1/station_info?share_key=eq.{SH
   -H "apikey: SUPABASE_ACCESS_TOKEN" \
   -H "Authorization: Bearer SUPABASE_ACCESS_TOKEN" \
   -H "Accept: application/vnd.pgrst.object+json"
+
+
+# 7. POST location/together
+curl -X POST http://localhost:54321/functions/v1/location-together \
+  -H "Authorization: Bearer SUPABASE_ACCESS_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "김동환",
+    "region_name": "A동",
+    "start_x": 127.129347097241,
+    "start_y": 37.4860140096538
+  }'
